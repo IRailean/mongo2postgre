64a3ef32932e6e68a52e70c887709a7c8ceede5d
Igor Valeriu Railean <igor-valeriu.railean@intern.arobs.com>
2020-05-16 20:32:24 +0300

    Change credentials

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ DEBUG = ENVIRONMENT == "dev"
 HOST = '0.0.0.0' if ENVIRONMENT == "prod" else 'localhost'
 POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "Motocross971")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "admin")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
 
